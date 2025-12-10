98ce48bdf09c63e8e8846d2a349d60446d86693f
TEE MING ZE <167508374+heymz1@users.noreply.github.com>
2025-12-10 15:26:49 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cn45ltk4itsh.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "teemingze-employee"
+customregion = "us-east-1"
+
